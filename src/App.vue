51d732a2596053eb78855e8e911f92745104c8b6
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-06-18 10:59:26 +0200

    Install less

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div 
+        ref="container"
+        class="container"
+        :class="{ 'dark-theme' : theme == 'dark',
+                  'light-theme' : theme == 'light' }"
+    >
+        <h1>Hello, I am Maciek</h1>
+        <h2>Software developer</h2>
+        <button @click="changeTheme();">Theme switch</button>
+    </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-    name: 'App',
+    name: 'Portfolio',
     components: {
-        HelloWorld
+
+    },
+    data() {
+        return {
+            theme: 'dark'
+        }
+    },
+    methods: {
+        changeTheme() {
+            if (this.theme == 'dark') {
+                this.$refs.container.classList.remove('dark-theme');
+                this.$refs.container.classList.add('light-theme');
+                this.theme = 'light';
+            } else {
+                this.$refs.container.classList.remove('light-theme');
+                this.$refs.container.classList.add('dark-theme');
+                this.theme = 'dark';
+            }
+        }
     }
 }
 </script>
 
-<style lang="scss">
+<style lang="less">
+@import '@/styles/variables.less';
+@import '@/styles/main.less';
+
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+    .container {
+        min-height: 100vh;
+    }
+
+    .dark-theme {
+        background-color: #dark[background-color];
+        color: #dark[text-color];
+    }
+        
+    .light-theme {
+        background-color: #light[background-color];
+        color: #light[text-color];
+    }
+        
 }
 </style>
