40ecfba8955feae30b9dba8d1fdf360d3bc11547
maciekiwaniuk <iwaniukmaciej.kontakt@gmail.com>
2024-10-29 21:02:12 +0100

    Update deployment script

--- docker/ssl/create_certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-openssl req -x509 -nodes -newkey rsa:2048 \
-  -config ./openssl.cnf \
-  -keyout privkey.pem \
-  -out fullchain.pem \
-  -days 3600 \
-  -subj '/C=DE/ST=NRW/L=Bielefeld/O=isocapi. KG/OU=fsj /CN=isocapi.dev/emailAddress=isoc.software@gmail.com'
